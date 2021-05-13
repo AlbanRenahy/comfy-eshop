708ef27423cb59c18b9c43478ef91f650d2c5980
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-13 08:44:31 +0200

    Set router in App.js and working correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import { AboutPage, CartPage, CheckoutPage, ErrorPage, HomePage, Produ}
+import {Home, SingleProduct, Cart, Checkout, Error, About, Products} from './pages'
 
 function App() {
   return (
-    <h4>comfy sloth</h4>)
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />}/>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+    )
 }
 
 export default App
