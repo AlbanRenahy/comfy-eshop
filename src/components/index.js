d2b8ddb12177b0101db2d1d8eb6ae11fe2df03ae
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 07:35:20 +0200

    Hero page and about page done

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import CartButtons from "./CartButtons";
-export { Navbar, Sidebar, Footer, CartButtons };
+import PageHero from "./PageHero";
+export { Navbar, Sidebar, Footer, CartButtons, PageHero };
