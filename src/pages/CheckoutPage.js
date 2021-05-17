62bfd580ef5ff124bb4257475d24b5874db63101
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 07:40:30 +0200

    Checkout page created

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+
+import { Link } from "react-router-dom";
 
 const CheckoutPage = () => {
-  return <h4>Checkout page</h4>
-}
-export default CheckoutPage
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <h1>Checkout here</h1>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`;
+export default CheckoutPage;
