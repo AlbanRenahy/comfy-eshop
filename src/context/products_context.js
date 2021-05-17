3a6508289c7fd174d24b4707b6db1d6f2309c708
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 15:55:48 +0200

    Products fetched from API

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,11 +1,21 @@
 import axios from "axios";
 import React, { useContext, useReducer, useEffect } from "react";
-import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
+import {
+  SIDEBAR_CLOSE,
+  SIDEBAR_OPEN,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from "../actions";
 import reducer from "../reducers/products_reducer";
 import { products_url as url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -21,8 +31,14 @@ export const ProductsProvider = ({ children }) => {
   };
 
   const fetchProducts = async (url) => {
-    const response = await axios.get(url);
-    console.log(response);
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({type:GET_PRODUCTS_SUCCESS, payload:products})
+    } catch (error) {
+      dispatch({type:GET_PRODUCTS_ERROR})
+    }
   };
 
   useEffect(() => {
