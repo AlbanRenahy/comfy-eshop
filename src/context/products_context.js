26e351bcdf5624deae5e2d5c63debf1cba2f78ac
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-17 08:58:28 +0200

    Fetching the products list using context

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 import React, { useContext, useReducer, useEffect } from "react";
 import { SIDEBAR_CLOSE, SIDEBAR_OPEN } from "../actions";
 import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 
 const initialState = {
   isSidebarOpen: false,
@@ -17,6 +19,16 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    const response = await axios.get(url);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
+
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
