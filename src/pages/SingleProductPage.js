a181604c1b6f9e2b236fb20e05e1a46244e6ec83
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-18 08:40:31 +0200

    Single product - Loading, Error

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,7 +1,40 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import styled from "styled-components";
+import { useProductsContext } from "../context/products_context";
+import { single_product_url as url } from "../utils/constants";
+import { formatPrice } from "../utils/helpers";
+import { Link } from "react-router-dom";
+
+import { Loading, Error } from "../components";
 
 const SingleProductPage = () => {
-  return <h4>SingleProduct page</h4>
-}
-export default SingleProductPage
+  const { id } = useParams();
+  const history = useHistory();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push("/");
+      }, 3000);
+    }
+    // eslint-disable-next-line
+  }, [error]);
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+  return <h4>SingleProduct page</h4>;
+};
+export default SingleProductPage;
