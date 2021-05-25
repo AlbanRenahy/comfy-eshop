25df7b83f364dd9607bf2e7874098dde3ff264db
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-25 13:56:36 +0200

    Add logic to sort products in sort component

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useContext, useReducer } from "react";
 import reducer from "../reducers/filter_reducer";
-import { LOAD_PRODUCTS, SET_GRIDVIEW, SET_LISTVIEW, UPDATE_SORT } from "../actions";
+import {
+  LOAD_PRODUCTS,
+  SET_GRIDVIEW,
+  SET_LISTVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  FILTER_PRODUCTS,
+} from "../actions";
 import { useProductsContext } from "./products_context";
 
 const initialState = {
@@ -8,6 +15,7 @@ const initialState = {
   all_products: [],
   grid_view: true,
   sort: "price-lowest",
+  filters: {},
 };
 
 const FilterContext = React.createContext();
@@ -19,6 +27,11 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
+
+  useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS });
+    dispatch({ type: SORT_PRODUCTS });
+  }, [state.sort, state.filters]);
 
   const setGridView = () => {
     dispatch({ type: SET_GRIDVIEW });
