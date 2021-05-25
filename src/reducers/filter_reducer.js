7c888f520b425a922625c59592ca263b652c477a
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-25 16:38:00 +0200

    Set the different filters logics in the reducer

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -5,9 +5,9 @@ import {
   UPDATE_SORT,
   SORT_PRODUCTS,
   UPDATE_FILTERS,
+  FILTER_PRODUCTS,
   CLEAR_FILTERS,
-  FILTER_PRODUCTS
-} from "../actions";
+} from '../actions'
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
@@ -18,21 +18,21 @@ const filter_reducer = (state, action) => {
       all_products: [...action.payload],
       filtered_products: [...action.payload],
       filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
-    };
+    }
   }
   if (action.type === SET_GRIDVIEW) {
-    return { ...state, grid_view: true };
+    return { ...state, grid_view: true }
   }
   if (action.type === SET_LISTVIEW) {
-    return { ...state, grid_view: false };
+    return { ...state, grid_view: false }
   }
   if (action.type === UPDATE_SORT) {
-    return { ...state, sort: action.payload };
+    return { ...state, sort: action.payload }
   }
   if (action.type === SORT_PRODUCTS) {
-    const { sort, filtered_products } = state;
-    let tempProducts = [];
-    if (sort === "price-lowest") {
+    const { sort, filtered_products } = state
+    let tempProducts = []
+    if (sort === 'price-lowest') {
       tempProducts = filtered_products.sort((a, b) => {
         // if (a.price < b.price) {
         //   return -1
@@ -41,10 +41,10 @@ const filter_reducer = (state, action) => {
         //   return 1
         // }
         // return 0
-        return a.price - b.price;
-      });
+        return a.price - b.price
+      })
     }
-    if (sort === "price-highest") {
+    if (sort === 'price-highest') {
       tempProducts = filtered_products.sort((a, b) => {
         // if (b.price < a.price) {
         //   return -1
@@ -53,25 +53,25 @@ const filter_reducer = (state, action) => {
         //   return 1
         // }
         // return 0
-        return b.price - a.price;
-      });
+        return b.price - a.price
+      })
     }
-    if (sort === "name-a") {
+    if (sort === 'name-a') {
       tempProducts = filtered_products.sort((a, b) => {
-        return a.name.localeCompare(b.name);
-      });
+        return a.name.localeCompare(b.name)
+      })
     }
-    if (sort === "name-z") {
+    if (sort === 'name-z') {
       tempProducts = filtered_products.sort((a, b) => {
-        return b.name.localeCompare(a.name);
-      });
-    }
-    if (action.type === UPDATE_FILTERS) {
-      const { name, value } = action.payload
-      return { ...state, filters: { ...state.filters, [name]: value } }
+        return b.name.localeCompare(a.name)
+      })
     }
 
-    return { ...state, filtered_products: tempProducts };
+    return { ...state, filtered_products: tempProducts }
+  }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload
+    return { ...state, filters: { ...state.filters, [name]: value } }
   }
   if (action.type === FILTER_PRODUCTS) {
     const { all_products } = state
@@ -82,6 +82,28 @@ const filter_reducer = (state, action) => {
         product.name.toLowerCase().startsWith(text)
       )
     }
+    if (category !== 'all') {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      )
+    }
+    if (company !== 'all') {
+      tempProducts = tempProducts.filter(
+        (product) => product.company === company
+      )
+    }
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find((c) => c === color)
+      })
+    }
+    // filter by price
+    tempProducts = tempProducts.filter((product) => product.price <= price)
+    // filter by shipping
+    if (shipping) {
+      tempProducts = tempProducts.filter((product) => product.shipping === true)
+    }
+    return { ...state, filtered_products: tempProducts }
   }
   if (action.type === CLEAR_FILTERS) {
     return {
@@ -97,8 +119,7 @@ const filter_reducer = (state, action) => {
       },
     }
   }
-  return state;
-  throw new Error(`No Matching "${action.type}" - action type`);
-};
+  throw new Error(`No Matching "${action.type}" - action type`)
+}
 
-export default filter_reducer;
+export default filter_reducer
