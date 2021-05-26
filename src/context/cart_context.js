1e312058eb96e99dd87cf2ae8cd96b3415839988
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-26 09:06:16 +0200

    Cart page setup

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -23,8 +23,18 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
+
+  const removeItem = (id) => {};
+
+  const toggleAmount = (id, value) => {};
+
+  const clearCart = () => {};
   return (
-    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
   );
 };
 // make sure use
